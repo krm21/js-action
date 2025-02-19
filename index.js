67445edc829b715ac231a990853167ac2cda3bc5
krm21 <kareemali21@gmail.com>
2025-02-19 18:04:28 +0100

    afds

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');
 try {
     // Get input from the action
     const message = core.getInput('message');
+    console.log(`${message}${message}${message}${message}`)
     
     // Log the message (GitHub Actions supports ::notice for highlighting)
     console.log(`::notice ::${message}`);
